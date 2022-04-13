2689605d29fae168899253e610396b8ee046995a
vishall <vishal.loke@gmail.com~>
2022-04-13 15:43:08 +0100

    first one

---
+++ script.js
@@ -0,0 +1,9 @@
+ let js = 'js';
+    if (js === 'js') alert('deal with it');
+    console.log(40 + 8 + 23 - 10);
+
+    let country = 'India';
+    let continent = 'Asia';
+    let population = '120B';
+
+    console.log(country,continent,population);
